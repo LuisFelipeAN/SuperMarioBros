2abff5950aeefcbac5d41b193986567e54a170a9
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-12-01 03:01:26 -0200

    alteracao sprites movimentacao colisao e movimentacao camera

--- Assets/ScriptMario.cs
+++ Assets/ScriptMario.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class ScriptMario : MonoBehaviour {
     public float velocidadeLateral=3;
     public float inpulsoPulo=7.5f;
+    public float minX;
 
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidbody2D;
     private bool andando;
+    private bool onFloor,direitaOcupada,esquerdaOcupada;
+    private BoxCollider2D boxCollider2D;
+
+
     public float GetVelocidade
     {
         get {
@@ -27,22 +32,28 @@ public class ScriptMario : MonoBehaviour {
        
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody2D=GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        onFloor = false;
     }
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        atualizaRayCasts();
+
+        if (Input.GetKeyDown(KeyCode.Space)&& onFloor)
         {
             rigidbody2D.AddForce(Vector3.up * inpulsoPulo);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow)){
+        if (Input.GetKey(KeyCode.RightArrow)&& !direitaOcupada)
+        {
           transform.Translate(Vector3.right * velocidadeLateral * Time.deltaTime);
           andando = true;
 
 
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow)&& !esquerdaOcupada&&transform.position.x-spriteRenderer.bounds.size.x/2>minX)
         {
             transform.Translate(Vector3.left * velocidadeLateral * Time.deltaTime);
             andando = true;
@@ -52,9 +63,18 @@ public class ScriptMario : MonoBehaviour {
             andando = false;
 
         }
-            transform.rotation = Quaternion.identity;
+        transform.rotation = Quaternion.identity;
     }
-   
+
+    public void atualizaRayCasts()
+    {
+        boxCollider2D.enabled = false;
+        onFloor = Physics2D.Raycast(transform.position, Vector2.down, spriteRenderer.bounds.size.y / 2 + 0.03f);
+        direitaOcupada = Physics2D.Raycast(transform.position, Vector2.right, spriteRenderer.bounds.size.x / 2 + 0.03f);
+        esquerdaOcupada = Physics2D.Raycast(transform.position, Vector2.left, spriteRenderer.bounds.size.x / 2 + 0.03f);
+        boxCollider2D.enabled = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,47 +14,37 @@ public class CameraController : MonoBehaviour {
 	public float cameraSpeed;					// velocidade da câmera (usada somente quando a câmera precisa se mover mais rapidamente do que o jogador)
 	public float backgroundSpeed;				// velocidade de movimentacão do fundo (deve ser diferente da velocidade da câmera principal para criar a sensacão de profundidade)
 	public bool alternativeBackground;			// apenas uma variável para permitir selecionar uma forma alternativa (e não a mais ideal) de trabalhar o efeito de paralaxe
+    private Vector3 leftBorderCam;
 
 	// Use this for initialization
 	void Start () {
-		// Pega o objeto controlado pelo jogador, que deve estar definido com a tag "Player" (poderia ter sido passado via Inspector também)
-		player = GameObject.FindGameObjectWithTag ("Player");
+        // Pega o objeto controlado pelo jogador, que deve estar definido com a tag "Player" (poderia ter sido passado via Inspector também)
+        
+        player = GameObject.FindGameObjectWithTag ("Player");
 
 		// Pega o script do jogador através do componente (o ideal seria criar uma classe separada para o jogador que fosse acessada tanto pelo CharacterScript quanto pelo CameraController)
 		playerScript = player.GetComponent<ScriptMario> ();
 
-		// Uma forma de alternar entre as duas solucões para a movimentacão de fundo (na prática, só uma seria usada, só foi feito isso para o exemplo da aula)
-		if(alternativeBackground)
-			// Neste caso, o fundo será movimentado através da câmera traseira (que foi devidamente marcada com a tag "Background")
-			backgroundController = GameObject.FindGameObjectWithTag ("Background").GetComponent<Camera>();
-		else
-			// Neste caso, a câmera traseira fica fixa e a textura aplicada sobre o objeto é deslocada com padrão de repeticão ativado (o Quad que contém a textura foi marcado com a tag "Background")
-			backgroundController = GameObject.FindGameObjectWithTag ("Background").GetComponent<MeshRenderer> ();
-	}
+		
+		// Neste caso, a câmera traseira fica fixa e a textura aplicada sobre o objeto é deslocada com padrão de repeticão ativado (o Quad que contém a textura foi marcado com a tag "Background")
+		backgroundController = GameObject.FindGameObjectWithTag ("Background").GetComponent<MeshRenderer> ();
+
+        leftBorderCam = new Vector3(0, 0, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		// Obtém a posicão do jogador em coordenadas de tela 
 		Vector3 screenPos = Camera.main.WorldToScreenPoint (player.transform.position);
-        int direction;
-        if (Input.GetKey(KeyCode.RightArrow))
-            direction = 1;
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            direction = -1;
-        else
-            direction = 0;
-
-        Vector3 dx = Vector3.right * direction *  Time.deltaTime;
+        Vector3 dx = Vector3.right *  Time.deltaTime;
         if (screenPos.x >= Screen.width /2)
         {
             Camera.main.transform.position += dx * playerScript.GetVelocidade;
-        }
-       
-        if (playerScript.GetVelocidade != 0)
-        {
+
             Renderer backRenderer = backgroundController as Renderer;
             backRenderer.material.mainTextureOffset += new Vector2(dx.x * backgroundSpeed, 0);
+
+            playerScript.minX = Camera.main.ScreenToWorldPoint(leftBorderCam).x;
         }
-       
     }
 }
